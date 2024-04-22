cf02f2cd732a1c1679feebccd054e640a56bbbdd
Mike Kogan <mikekogan@mikes-air.mynetworksettings.com>
2024-04-22 02:48:32 -0400

    updates

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -23,7 +23,7 @@ export function Sidebar({setData, onSelectKeyword}) {
               <div className="fixed-sidebar space-y-6">
 
               <div className="flex h-14 items-center px-6 py-4 lg:h-auto">
-                <a className="group flex w-full items-center gap-x-2.5" onClick={() => setData(homepage)}>
+                <a className="group flex w-full items-center gap-x-2.5" onClick={() => onSelectKeyword('')}>
                     <div className="h-7 w-7 rounded-full border">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="h-6 w-6"><rect width="256" height="256" fill="none"></rect><line x1="208" y1="128" x2="128" y2="208" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
                             <line x1="192" y1="40" x2="40" y2="192" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
