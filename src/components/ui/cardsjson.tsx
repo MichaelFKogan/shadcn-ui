dd9bc687911de6b6eb5e9579e74f14527b9908c8
Mike Kogan <mikekogan@mikes-air.mynetworksettings.com>
2024-04-23 20:17:08 -0400

    updates

--- src/components/ui/cardsjson.tsx
+++ src/components/ui/cardsjson.tsx
@@ -62,21 +62,30 @@ export function CardsJson({ data }) {
                                     <a href={item.url} target="_blank" key={index}>
                                         <div className="card bg-surface-100 hover:bg-surface-200 group flex h-full w-full flex-col rounded-xl border px-4 py-4 shadow transition-all hover:shadow-lg">
                                             <div className="flex flex-col">
-                                                <div className="flex w-full space-x-6" style={{ alignItems: "center" }}>
+                                                <div className="flex w-full" style={{ alignItems: "flex-start" }}>
                                                     <div className="relative h-[65px] min-w-[65px] w-[65px] rounded-md overflow-hidden scale-100 transition-all">
                                                         <img alt={item.title} loading="lazy" decoding="async" data-nimg="fill" className="bg-surface-100" style={{ position: "absolute", height: "100%", width: "100%", left: "0", top: "0", right: "0", bottom: "0", objectFit: "cover", color: "transparent" }} sizes="100vw" src={imagePath} />
                                                     </div>
-                                                    <div className="card__text-wrapper">
+                                                    <div className="card__text-wrapper ml-4">
                                                         <h3 className="text-foreground-light group-hover:text-foreground mb-2 text-xl font-medium transition-colors">{item.title}</h3>
                                                         <p className="text-foreground-lighter text-xs line-clamp-4 min-h-[50px] h-[58px]" title={item.description}>{item.description}</p>
+                                                        
+                                                        {/* <div className="flex justify-end mt-2">
+                                                            {item.category ? <Badge variant="secondary" className="badge badge-category text-xs">{item.category}</Badge> : null}
+
+                                                            {item.city ? <Badge variant="outline" className="badge badge-city text-xs ml-2">{item.city}</Badge> :
+                                                            item.country ? <Badge variant="outline" className="badge badge-country text-xs ml-2">{item.country}</Badge> : 
+                                                            item.continent ? <Badge variant="outline" className="badge badge-continent text-xs ml-2">{item.continent}</Badge> : null}
+                                                        </div> */}
                                                     </div>
                                                 </div>
-                                                <div className="flex justify-end">
-                                                    {item.category ? <Badge variant="secondary" className="badge badge-category text-xs mt-2">{item.category}</Badge> : null}
+                                                <hr className="mt-4"/>
+                                                <div className="flex justify-center badge-wrapper mt-2">
+                                                    {item.category ? <Badge variant="secondary" className="badge badge-category mt-2 text-xs">{item.category}</Badge> : null}
 
-                                                    {item.city ? <Badge variant="outline" className="badge badge-city text-xs mt-2 ml-2">{item.city}</Badge> :
-                                                    item.country ? <Badge variant="outline" className="badge badge-country text-xs mt-2 ml-2">{item.country}</Badge> : 
-                                                    item.continent ? <Badge variant="outline" className="badge badge-continent text-xs mt-2 ml-2">{item.continent}</Badge> : null}
+                                                    {item.city ? <Badge variant="outline" className="badge badge-city mt-2 text-xs">{item.city}</Badge> :
+                                                    item.country ? <Badge variant="outline" className="badge badge-country mt-2 text-xs">{item.country}</Badge> : 
+                                                    item.continent ? <Badge variant="outline" className="badge badge-continent mt-2 text-xs">{item.continent}</Badge> : null}
                                                 </div>
                                             </div>
                                         </div>
