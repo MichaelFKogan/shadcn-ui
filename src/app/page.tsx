f7e76751a46bded64dfa1ffcf7d0b247da1a11a7
Mike Kogan <mikekogan@Mikes-MacBook-Air.local>
2024-05-06 14:19:06 -0400

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import jsonData from '../data/data.json';
 
 export default function Home() {
 
-const [filterKeyword, setFilterKeyword] = useState('');
+const [filterKeyword, setFilterKeyword] = useState("instagram youtube tiktok reddit blog vlog");
 const [lastKeyword, setLastKeyword] = useState('');  // State to track the last keyword
 const [combinedKeyword, setCombinedKeyword] = useState('');
 
@@ -24,7 +24,7 @@ const [secondBreadcrumbKeyword, setSecondBreadcrumbKeyword] = useState('');
 const [listData, setListData] = useState([]);
 const [tableData, setTableData] = useState([]);
 const [uniqueCategories, setUniqueCategories] = useState(new Set());
-const [isSwitchOn, setIsSwitchOn] = useState(true);
+const [isSwitchOn, setIsSwitchOn] = useState(false);
 const [sidebarOpen, setSidebarOpen] = useState(true);
 const [hideBanner, setHideBanner] = useState(false);
 const [selectedBreadcrumb, setSelectedBreadcrumb] = useState('first');
