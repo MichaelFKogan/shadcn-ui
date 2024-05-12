65a9f361b761ec5aa1f076b99317a4e5372b5fc2
Mike Kogan <mikekogan@mikes-air.mynetworksettings.com>
2024-05-12 16:30:57 -0400

    updates

--- src/components/Views.tsx
+++ src/components/Views.tsx
@@ -25,7 +25,6 @@ export function Views({
     combinedKeyword,
     breadcrumbKeyword, 
     secondBreadcrumbKeyword, 
-    continentBreadcrumbKeyword, 
     setBreadcrumbKeyword, 
     setSecondBreadcrumbKeyword, 
     clearKeywords,
